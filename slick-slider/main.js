f61ce93eb26516a4e47d2730b76c1675818b38ae
simantabaral <baralsimanta@gmail.com>
2020-05-13 19:25:51 +0545

    slick-slider

---
+++ slick-slider/main.js
@@ -0,0 +1,9 @@
+ $(document).ready(function(){
+      $('.slider .slider-item').slick({
+        autoplay:true,
+        dots:true,
+        autoplaySpeed:3000,
+        prevArrow:'.arrow .left',
+        nextArrow:'.arrow .right'
+      });
+    });
